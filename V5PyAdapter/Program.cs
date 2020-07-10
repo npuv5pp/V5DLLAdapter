adfbf44252aa1821c65a50dd297646428106477b
Peng Guanwen <pg999w@outlook.com>
2020-07-10 21:05:09 +0800

    加入PyAdapter项目

---
+++ V5PyAdapter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Python.Runtime;
+
+namespace V5PyAdapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = args[0];
+            Console.WriteLine("Hello World!");
+            using (Py.GIL())
+            {
+                dynamic sys = Py.Import("sys");
+                sys.path.append(".");
+                
+                dynamic strategy = Py.Import(name);
+                Console.WriteLine(strategy.get_team_info(1));
+            }
+        }
+    }
+}
