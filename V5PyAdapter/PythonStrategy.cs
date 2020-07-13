43d3c0924a0b9aa0d91fe87fcbe82b4c7d6f3cd8
ck <18038947419@163.com>
2020-07-13 13:46:56 +0800

    make python interface usable

--- V5PyAdapter/PythonStrategy.cs
+++ V5PyAdapter/PythonStrategy.cs
@@ -14,9 +14,8 @@ namespace V5PyAdapter
             using (Py.GIL())
             {
                 dynamic sys = Py.Import("sys");
-                sys.path.append(".");
-
-                strategy = Py.Import(name);
+                sys.path.insert(0,"");
+                strategy = Py.Import(name);                
             }
         }
 
@@ -25,7 +24,20 @@ namespace V5PyAdapter
             using (Py.GIL())
             {
                 dynamic result = strategy.get_instruction(field);
-                return result;
+                Wheel[] wheels = new Wheel[5];
+                for (int index = 0; index < 5; index++)
+                {
+                    wheels[index] = new Wheel()
+                    {
+                        LeftSpeed = result[0][index][0],
+                        RightSpeed = result[0][index][1]
+                    };
+                }
+                ControlInfo controlInfo = new ControlInfo
+                {
+                    Command = result[1]
+                };
+                return (wheels,controlInfo);
             }
         }
 
@@ -34,15 +46,27 @@ namespace V5PyAdapter
             using (Py.GIL())
             {
                 dynamic result = strategy.get_placement(field);
-                return result;
+                V5RPC.Proto.Placement placement = new Placement();
+                for (int index = 0; index < 5; index++)
+                {
+                    placement.Robots.Add(new Robot
+                    {
+                        Position = new Vector2 { X = result[index][0], Y = result[index][1] },
+                        Rotation = result[index][2]
+                    });
+                }
+                placement.Ball = new Ball {
+                    Position = new Vector2 { X = result[5][0], Y = result[5][1] }
+                };
+                return placement;
             }
         }
 
-        public TeamInfo GetTeamInfo(ServerInfo info)
+        public TeamInfo GetTeamInfo(V5RPC.Proto.Version info)
         {
             using (Py.GIL())
             {
-                string teamName = strategy.get_team_info();
+                string teamName = strategy.get_team_info(info);
                 return new TeamInfo { TeamName = teamName };
             }
         }
