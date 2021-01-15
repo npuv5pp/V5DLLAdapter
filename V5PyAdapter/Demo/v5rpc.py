42aacd29d1747d1d87313393fe96a3207b70ef8b
Eternity <2536890272@qq.com>
2021-01-15 19:36:12 +0800

    修改了初值0变量的存在性

--- V5PyAdapter/Demo/v5rpc.py
+++ V5PyAdapter/Demo/v5rpc.py
@@ -17,6 +17,9 @@ def catchall(fun):
 
     return inner
 
+def hasattr(obj, attr: str) -> bool:
+    attrs = dir(obj)
+    return not attr in attrs
 
 class Team:
     Self = 0
@@ -59,26 +62,64 @@ class Version:
 
 
 class Vector2:
+    def __init__(self, v) -> None:
+        if hasattr(v, "x"):
+            self.x = 0
+        else:
+            self.x = v.x
+
+        if hasattr(v, "y"):
+            self.y = 0
+        else:
+            self.y = v.y
     x: float
     y: float
 
 
 class Wheel:
+    def __init__(self, whell):
+        if hasattr(whell, "LeftSpeed"):
+            self.LeftSpeed = 0
+        else:
+            self.LeftSpeed = whell.LeftSpeed
+
+        if hasattr(whell, "RightSpeed"):
+            self.RightSpeed = 0
+        else:
+            self.RightSpeed = whell.RightSpeed
     LeftSpeed: float
     RightSpeed: float
 
 
 class Robot:
+    def __init__(self, robot):
+        self.Position = Vector2(robot.Position)
+        if hasattr(robot, "Rotation"):
+            self.Rotation = 0
+        else:
+            self.Rotation=robot.Rotation
+        self.Wheel = Wheel(robot.Wheel)
     Position: Vector2
     Rotation: float
     Wheel: Wheel
 
 
 class Ball:
+    def __init__(self, ball) -> None:
+        self.Position = Vector2(ball.Position)
     Position: Vector2
 
 
 class Field:
+    def __init__(self, field):
+        self.SelfRobots = []
+        for i in field.SelfRobots:
+            self.SelfRobots.append(Robot(i))
+        self.OpponentRobots = []
+        for i in field.OpponentRobots:
+            self.OpponentRobots.append(Robot(i))
+        self.Ball = Ball(field.Ball)
+        self.Tick = field.Tick
     SelfRobots: List[Robot]
     OpponentRobots: List[Robot]
     Ball: Ball
